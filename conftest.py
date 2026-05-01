55874a29eb72ba211a256117aef3a39a9de56d5a
Peterson93 <peterson.delarosa93@gmail.com>
2026-05-01 18:23:09 -0500

    fix:corrige typo en workflow

--- conftest.py
+++ conftest.py
@@ -13,10 +13,11 @@ def driver():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
+    options.add_argument("--remote-debugging-port=9222")
 
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
-    
+    driver = webdriver.Chrome(service=service, options=options)
+
     driver.maximize_window()
     yield driver
     driver.quit()
