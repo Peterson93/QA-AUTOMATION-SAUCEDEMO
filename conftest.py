4bfd7fbd43bc3e8e047d27c3ac1b7ff99f69fc78
Peterson93 <peterson.delarosa93@gmail.com>
2026-05-01 17:50:01 -0500

    fix: install chrome in CI

--- conftest.py
+++ conftest.py
@@ -2,12 +2,21 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from utils.screenshot import take_screenshot
+from selenium.webdriver.chrome.options import options
 import pytest
 
 @pytest.fixture
 def driver():
+    options = options()
+
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
+    
     driver.maximize_window()
     yield driver
     driver.quit()
